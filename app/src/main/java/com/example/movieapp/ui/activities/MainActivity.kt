554ff11f38a4ed7b2b33636843af702f476bef9b
Fabio Cavallari <fabio.cavallari94@gmail.com>
2024-07-24 16:01:52 -0300

    add top bar

--- app/src/main/java/com/example/movieapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/ui/activities/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.movieapp.R
 import com.example.movieapp.model.movieListSample
 import com.example.movieapp.ui.screens.HomeScreen
 import com.example.movieapp.ui.states.HomeScreenUiState
@@ -30,11 +36,26 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App(content: @Composable () -> Unit = {}) {
     MovieAppTheme {
         Surface {
-            Scaffold { paddingValues ->
+            Scaffold(
+                topBar = {
+                    CenterAlignedTopAppBar(
+                        title = {
+                            Row(
+                                Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Center,
+                            ) {
+                                Text(
+                                    text = stringResource(id = R.string.app_name),
+                                )
+                            }
+                        },)
+                }
+            ) { paddingValues ->
                 Box(
                     Modifier.padding(paddingValues)
                 ) {
