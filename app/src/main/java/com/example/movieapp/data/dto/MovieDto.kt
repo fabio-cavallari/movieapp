24568ddfd6ab29e27cd0ba8d430649e70fac9ca6
Fabio Cavallari <fabio.cavallari94@gmail.com>
2024-07-25 11:33:53 -0300

    fix JsonIgnoreProperties

--- app/src/main/java/com/example/movieapp/data/dto/MovieDto.kt
+++ app/src/main/java/com/example/movieapp/data/dto/MovieDto.kt
@@ -1,7 +1,9 @@
 package com.example.movieapp.data.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class MovieDto(
     @JsonProperty("title") val title: String,
     @JsonProperty("poster_path") val poster: String,
