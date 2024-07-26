7537e486a037f668359e35bb8996b6e6712ae6e1
Fabio Cavallari <fabio.cavallari94@gmail.com>
2024-07-25 22:11:39 -0300

    using koin client instance

--- app/src/main/java/com/example/movieapp/di/AppModule.kt
+++ app/src/main/java/com/example/movieapp/di/AppModule.kt
@@ -2,18 +2,50 @@ package com.example.movieapp.di
 
 import com.example.movieapp.data.repositories.MovieListRepository
 import com.example.movieapp.data.repositories.MovieRepositoryImpl
+import com.example.movieapp.network.clients.MovieDbClient
 import com.example.movieapp.network.remoteproviders.MovieDbRemoteProvider
 import com.example.movieapp.network.remoteproviders.MovieDbRemoteProviderImpl
 import com.example.movieapp.viewmodels.MovieListViewModel
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.factoryOf
+import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
 
 object AppModule {
     val modules = module {
         factoryOf(::MovieDbRemoteProviderImpl) { bind<MovieDbRemoteProvider>() }
         factoryOf(::MovieRepositoryImpl) { bind<MovieListRepository>() }
         viewModelOf(::MovieListViewModel)
+
+        factory<OkHttpClient> {params ->
+            val headers = params.get<Map<String, String>>()
+            OkHttpClient.Builder()
+                .addInterceptor { chain ->
+                    val originalRequest: Request = chain.request()
+                    val builder = originalRequest.newBuilder()
+                    headers.forEach { header ->
+                        builder.addHeader(header.key, header.value)
+                    }
+                    chain.proceed(builder.build())
+                }
+                .build()
+        }
+
+        factory<MovieDbClient> {
+            val headersParams = {
+                parametersOf(mapOf("Authorization" to "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjE3N2UwNjM5Mzk3MTQ4Y2I2ZmNmMGI5MzAyYTY4ZCIsIm5iZiI6MTcyMTc2NzQ4Ni4wMTUxNTEsInN1YiI6IjYyNzg1YjUxYTgwMjM2MTQxNDYxMmJjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iVALUHYHr3Ah-zjKzWEeDLVNFKmq_WCg446M-g_HO0w"))
+            }
+            Retrofit.Builder()
+                .addConverterFactory(JacksonConverterFactory.create())
+                .baseUrl("https://api.themoviedb.org")
+                .client(get(parameters = headersParams))
+                .build()
+                .create(MovieDbClient::class.java)
+        }
     }
 }
