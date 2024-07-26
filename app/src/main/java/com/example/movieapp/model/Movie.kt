b3c6494e9a5a0a921cfb43170844bd42232a8e33
Fabio Cavallari <fabio.cavallari94@gmail.com>
2024-07-26 16:25:15 -0300

    override movie equals and hashCode

--- app/src/main/java/com/example/movieapp/model/Movie.kt
+++ app/src/main/java/com/example/movieapp/model/Movie.kt
@@ -8,7 +8,17 @@ data class Movie(
     val date: String,
     val poster: String,
     val rate: String
-)
+) {
+    //there were some cases where the api returns the same movie twice but with different rate 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Movie) return false
+        return id == other.id
+    }
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
 
 fun MovieDto.asDomainModel() = Movie(id, title, date, poster, rate)
 
