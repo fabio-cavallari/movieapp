5f640a14aa98feccc908b7b8b6b344157aa6d612
Fabio Cavallari <fabio.mesquita@americanas.io>
2024-07-29 01:55:05 -0300

    change app icon

--- app/src/main/java/com/example/movieapp/components/MovieCard.kt
+++ app/src/main/java/com/example/movieapp/components/MovieCard.kt
@@ -12,12 +12,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,7 +31,6 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
 import coil.compose.SubcomposeAsyncImage
 import com.example.movieapp.R
 import com.example.movieapp.model.Movie
@@ -95,7 +92,7 @@ fun MovieCard(movie: Movie, onMovieClick: (Movie) -> Unit = {}) {
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     Image(
-                        painter = painterResource(id = R.drawable.popcorn_svgrepo_com),
+                        painter = painterResource(id = R.drawable.ic_popcorn),
                         contentDescription = null
                     )
                     Spacer(modifier = Modifier.width(8.dp))
